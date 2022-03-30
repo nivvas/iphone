fdccdc6db06dc1c71be1e77c9363bbb460fb9191
nivvas <75578258+nivvas@users.noreply.github.com>
2022-03-30 19:56:40 +0400

    Update getData.js

--- scripts/getData.js
+++ scripts/getData.js
@@ -40,7 +40,7 @@ const getData = () => {
     }
 
     const getGoods = () => {
-        fetch('https://nivvas.github.io/cross-sell-dbase/dbase.json')
+        fetch('../dbase.json')
 
             .then((response) => {
                 if (response.ok) {
