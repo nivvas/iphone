e92db15b42cfa1ef3c3ae2c1b1a1789184afdf2c
nivvas <75578258+nivvas@users.noreply.github.com>
2022-04-06 20:02:56 +0400

    Update tubs.js
    js govno was made

--- scripts/tubs.js
+++ scripts/tubs.js
@@ -2,28 +2,28 @@ const tabsFunc = () => {
     const tabs = document.querySelectorAll('.card-detail__change')
     const tabsTitle = document.querySelector('.card-details__title')
     const tabsPrice = document.querySelector('.card-details__price')
-    const tabsImages = document.querySelector('.card__image_item')
+    const tabsImages = document.querySelectorAll('.card__image_item')
     const indexTitle = document.querySelector('title')
     const discription = document.querySelector('.description__memory')
 
-
+    
     const options = [
         {
             name: 'Graphite',
             memory: '128',
-            price: '100000',
+            price: '99 999',
             img: 'img/iPhone-graphite.webp'
         },
         {
             name: 'Silver',
             memory: '256',
-            price: '140999',
+            price: '139 999',
             img: 'img/iPhone-silver.webp'
         },
         {
             name: 'Sierra Blue',
             memory: '512',
-            price: '160999',
+            price: '159 999',
             img: 'img/iPhone-sierra_blue.webp'
         }
     ]
@@ -31,7 +31,9 @@ const tabsFunc = () => {
     const changeContent = (index) => {
         tabsTitle.textContent = `Смартфон Apple iPhone 13 Pro ${options[index].memory}GB ${options[index].name}`
         tabsPrice.textContent = `${options[index].price}₽`
-        tabsImages.setAttribute('src', options[index].img)
+        tabsImages.forEach((img) => {
+            img.setAttribute('src', options[index].img)
+        })
         indexTitle.textContent = `iPhone 13 Pro ${options[index].memory}GB ${options[index].name}`
         discription.textContent = `Встроенная память (ROM) ${options[index].memory} ГБ`
     }
